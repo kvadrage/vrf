kvadrage/vrf

commit b75ffc65f94aff6aaabc3f7ebaf3d7b619cdb789

vrf: Make mgmt vrf context the default if it exists
Add cl-vrf to the sudoers file; need to set context for cumulus user
when it logs in. Update cl-vrf to run through sudo.

Import ping and traceroute wrappers from cl-mgmtvrf package and update
for 3.0. Import postinst script as well.

Divert bashrc files from bash package that overwrite the PS1.

Closes: CM-9944

Signed-off-by: David Ahern <dsa@cumulusnetworks.com>

lib-cumulus/ping.cl
@@ -1,0 +1,24 @@
+#!/usr/bin/env bash
+
+vrf=$(/usr/cumulus/bin/cl-vrf id)
+
+# if we are not in mgmt VRF then run the command as given
+# in whatever VRF context we are in
+
+if [ "$vrf" != "mgmt" ]; then
+	exec /bin/ping.iputils-ping $*
+fi
+
+# we are in the mgmt VRF context. is -I eth0 given? if so
+# the user wants a ping in this context. nothing to do.
+
+echo "$*" | egrep -q -- '-I.*eth0|-I.*mgmt'
+if [ $? -eq 0 ]; then
+	exec /bin/ping.iputils-ping $*
+fi
+
+# this means we are in mgmt VRF and -I eth0 was not given.
+# Want ping to default to front panel ports using default VRF
+
+/usr/cumulus/bin/cl-vrf task set default $$
+exec /bin/ping.iputils-ping $*

lib-cumulus/ping6.cl
@@ -1,0 +1,26 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+vrf=$(/usr/cumulus/bin/cl-vrf id)
+
+# if we are not in mgmt VRF then run the command as given
+# in whatever VRF context we are in
+
+if [ "$vrf" != "mgmt" ]; then
+	exec /bin/ping6.iputils-ping $*
+fi
+
+# we are in the mgmt VRF context. is -I eth0 given? if so
+# the user wants a ping in this context. nothing to do.
+echo "$*" | egrep -q -- '-I.*eth0'
+if [ $? -eq 0 ]; then
+	exec /bin/ping6.iputils-ping $*
+fi
+
+# this means we are in mgmt VRF and -I eth0 was not given.
+# Want ping to default to front panel ports using default VRF
+
+/usr/cumulus/bin/cl-vrf task set default $$
+exec /bin/ping6.iputils-ping $*
+

lib-cumulus/traceroute.cl
@@ -1,0 +1,24 @@
+#!/usr/bin/env bash
+
+vrf=$(/usr/cumulus/bin/cl-vrf id)
+
+# if we are not in mgmt VRF then run the command as given
+# in whatever VRF context we are in
+
+if [ "$vrf" != "mgmt" ]; then
+	exec /usr/bin/traceroute.db $*
+fi
+
+# we are in the mgmt VRF context. is -i eth0 given? if so
+# the user wants a traceroute in this context. nothing to do.
+
+echo "$*" | egrep -q -- '-i.*eth0|-i.*mgmt'
+if [ $? -eq 0 ]; then
+	exec /usr/bin/traceroute.db $*
+fi
+
+# this means we are in mgmt VRF and -I eth0 was not given.
+# Want traceroute to default to front panel ports using default VRF
+
+/usr/cumulus/bin/cl-vrf task set default $$
+exec /usr/bin/traceroute.db $*

lib-cumulus/traceroute6.cl
@@ -1,0 +1,24 @@
+#!/usr/bin/env bash
+
+vrf=$(/usr/cumulus/bin/cl-vrf id)
+
+# if we are not in mgmt VRF then run the command as given
+# in whatever VRF context we are in
+
+if [ "$vrf" != "mgmt" ]; then
+	exec /usr/bin/traceroute.db -6 $*
+fi
+
+# we are in the mgmt VRF context. is -i eth0 given? if so
+# the user wants a traceroute in this context. nothing to do.
+
+echo "$*" | egrep -q -- '-i.*eth0|-i.*mgmt'
+if [ $? -eq 0 ]; then
+	exec /usr/bin/traceroute.db -6 $*
+fi
+
+# this means we are in mgmt VRF and -I eth0 was not given.
+# Want traceroute to default to front panel ports using default VRF
+
+/usr/cumulus/bin/cl-vrf task set default $$
+exec /usr/bin/traceroute.db -6 $*

debian/install
@@ -1,3 +1,4 @@
 cumulus-bin/*		usr/cumulus/bin
+lib-cumulus/*		usr/lib/cumulus
 systemd/*		lib/systemd/system
 etc/*			etc/

debian/postinst
@@ -1,0 +1,121 @@
+#!/bin/bash
+# postinst script for cl-vrf
+#
+# see: dh_installdeb(1)
+
+install_alt() {
+
+    # Installs the alternative, cumulus-specific file and moves the original 
+    # program to name "orig_name".<pkg-name>. For example, brctl is renamed 
+    # brctl.bridge-utils, start-stop-daemon is renamed start-stop-daemon.dpkg
+    # Example invocations of the routine:
+    #  install_alt /sbin/brctl /usr/lib/cumulus/brctl.cl
+    #  install_alt /sbin/start-stop-daemon /usr/lib/cumulus/start-stop-daemon.cl
+
+    local orig_file=$1
+    local alt_file=$2
+    local prg_name=`basename $1`
+    local no_alt=0
+
+    echo "Installing alternative $2 for $1 .. "
+
+    if [ "${orig_file:0:1}" != "/" ]; then
+	    echo "Specify fully qualified pathname for original file, $orig_file"
+        return 1
+    fi
+
+    # If alternative already exists, exit
+    [ -h $orig_file  -a -n "$(update-alternatives --query $prg_name 2>&1 | grep $alt_file)" ] && return 0;
+
+    if [ -n "$(update-alternatives --query $prg_name 2>&1 | grep 'error: no alternatives')" ]; then
+	    no_alt=1
+    fi
+
+    if [ "$no_alt" -eq 1 ]; then
+	    pkg_name=`dpkg -S $orig_file | cut -d':' -f 1`
+	    if [ -z $pkg_name ]; then
+		    echo "Failed to obtain package name for $orig_file"
+		    return 1
+	    fi
+
+	    mv $orig_file $orig_file.$pkg_name
+	    if [ $? -ne 0 ]; then
+		    echo "Failed to setup $prg_name alternatives"
+		    return 1
+	    fi
+
+	    update-alternatives --install $orig_file $prg_name $alt_file 20
+	    if [ $? -ne 0 ]; then
+		    echo "update-alternatives for alternative $alt_file failed with error " $?
+		    mv -f $orig_file.$pkg_name $orig_file
+		    return 1
+	    fi
+
+	    update-alternatives --install $orig_file $prg_name $orig_file.$pkg_name 10
+	    if [ $? -ne 0 ]; then
+		    echo "update-alternatives for original file $orig_file failed with error " $?
+		    update-alternatives --remove $prg_name $alt_file
+		    mv -f $orig_file.$pkg_name $orig_file
+		    return 1
+	    fi
+    else
+	    update-alternatives --install $orig_file $prg_name $alt_file 200
+	    if [ $? -ne 0 ]; then
+		    echo "update-alternatives for original file $orig_file failed with error " $?
+		    update-alternatives --remove $prg_name $alt_file
+		    mv -f $orig_file.$pkg_name $orig_file
+		    return 1
+	    fi
+	    return 0
+    fi
+}
+
+ARCH=`uname -m`
+ret=0
+
+set -e
+
+# summary of how this script can be called:
+#        * <postinst> `configure' <most-recently-configured-version>
+#        * <old-postinst> `abort-upgrade' <new version>
+#        * <conflictor's-postinst> `abort-remove' `in-favour' <package>
+#          <new-version>
+#        * <postinst> `abort-remove'
+#        * <deconfigured's-postinst> `abort-deconfigure' `in-favour'
+#          <failed-install-package> <version> `removing'
+#          <conflicting-package> <version>
+# for details, see http://www.debian.org/doc/debian-policy/ or
+# the debian-policy package
+
+chmod 4755 /usr/lib/cumulus/ping.cl
+chmod 4755 /usr/lib/cumulus/ping6.cl
+chmod 4755 /usr/lib/cumulus/traceroute.cl
+chmod 4755 /usr/lib/cumulus/traceroute6.cl
+
+# Install alternatives to ping and traceroute
+install_alt /bin/ping /usr/lib/cumulus/ping.cl
+install_alt /bin/ping6 /usr/lib/cumulus/ping6.cl
+install_alt /usr/bin/traceroute /usr/lib/cumulus/traceroute.cl
+install_alt /usr/bin/traceroute6 /usr/lib/cumulus/traceroute6.cl
+
+# We set PS1 in our profile.d script; remove the overrides by the
+# bash package
+for f in /etc/skel/.bashrc /etc/bash.bashrc
+do
+	# if file does not exist or we have already diverted move on
+	[ ! -f ${f} -o -e ${f}.cl_vrf ] && continue
+	dpkg-divert --divert ${f}.cl_vrf --rename ${f}
+	sed -e 's/PS1=/#PS1=/' ${f}.cl_vrf > ${f}
+done
+
+# fixup cumulus user bashrc
+if [ -f /home/cumulus/.bashrc ]; then
+	sed -ie 's/^PS1=/#PS1=/' /home/cumulus/.bashrc
+fi
+
+# dh_installdeb will replace this with shell code automatically
+# generated by other debhelper scripts.
+
+#DEBHELPER#
+
+exit 0

etc/profile.d/cl_vrf.sh
@@ -2,14 +2,14 @@
 # in a VRF context
 [ -n "$VRF" ] && exit 0
 
+# Name of the management VRF device
 MGMT_VRF=mgmt
-[ -e /etc/vrf.conf ] && . /etc/vrf.conf
 
 /usr/cumulus/bin/cl-vrf exists $MGMT_VRF
-if [ $? -eq 0 -a $UID -eq 0 ]; then
+if [ $? -eq 0 ]; then
 	VRF=$(/usr/cumulus/bin/cl-vrf identify prompt)
 	if [ -z "$VRF" ]; then
-		/usr/cumulus/bin/cl-vrf task set $MGMT_VRF $$
+		sudo /usr/cumulus/bin/cl-vrf task set $MGMT_VRF $$
 	fi
 fi
 

etc/sudoers.d/cumulus-vrf
@@ -1,0 +1,3 @@
+Defaults env_keep += VRF
+
+cumulus ALL = NOPASSWD: /usr/cumulus/bin/cl-vrf

commit a411dfd0346504a78358ba3e9d6c725603c77125

ping and traceroute wrappers should use sudo
The wrappers need to use sudo when calling cl-vrf to set the context.

Fixes: b75ffc65f94af ("vrf: Make mgmt vrf context the default if it exists")
Closes: CM-9944

Signed-off-by: David Ahern <dsa@cumulusnetworks.com>

lib-cumulus/ping.cl
@@ -20,5 +20,5 @@ fi
 # this means we are in mgmt VRF and -I eth0 was not given.
 # Want ping to default to front panel ports using default VRF
 
-/usr/cumulus/bin/cl-vrf task set default $$
+sudo /usr/cumulus/bin/cl-vrf task set default $$
 exec /bin/ping.iputils-ping $*

lib-cumulus/ping6.cl
@@ -21,6 +21,6 @@ fi
 # this means we are in mgmt VRF and -I eth0 was not given.
 # Want ping to default to front panel ports using default VRF
 
-/usr/cumulus/bin/cl-vrf task set default $$
+sudo /usr/cumulus/bin/cl-vrf task set default $$
 exec /bin/ping6.iputils-ping $*
 

lib-cumulus/traceroute.cl
@@ -20,5 +20,5 @@ fi
 # this means we are in mgmt VRF and -I eth0 was not given.
 # Want traceroute to default to front panel ports using default VRF
 
-/usr/cumulus/bin/cl-vrf task set default $$
+sudo /usr/cumulus/bin/cl-vrf task set default $$
 exec /usr/bin/traceroute.db $*

lib-cumulus/traceroute6.cl
@@ -20,5 +20,5 @@ fi
 # this means we are in mgmt VRF and -I eth0 was not given.
 # Want traceroute to default to front panel ports using default VRF
 
-/usr/cumulus/bin/cl-vrf task set default $$
+sudo /usr/cumulus/bin/cl-vrf task set default $$
 exec /usr/bin/traceroute.db -6 $*