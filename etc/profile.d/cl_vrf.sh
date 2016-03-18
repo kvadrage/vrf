ec3a8d6097cdfa1237522a0a130ae7ca188cf186
David Ahern <dsa@cumulusnetworks.com>
2016-03-18 15:31:17 -0600

    cl_vrf: Don't use exit in a profile script
    Curt reported that sudo -i as cumulus was failing. Root cause is that
    the cl_vrf.sh profile script was exiting because VRF environment variable
    was set. Since the profile scripts are sourced they can not call exit.
    
    Signed-off-by: David Ahern <dsa@cumulusnetworks.com>

--- etc/profile.d/cl_vrf.sh
+++ etc/profile.d/cl_vrf.sh
@@ -1,12 +1,12 @@
 # default task to management VRF if it exists and task is not running
 # in a VRF context
-[ -n "$VRF" ] && exit 0
-
-/usr/cumulus/bin/cl-vrf mgmt status >/dev/null 2>&1
-if [ $? -eq 0 ]; then
-	VRF=$(/usr/cumulus/bin/cl-vrf identify prompt)
-	if [ -z "$VRF" ]; then
-		sudo /usr/cumulus/bin/cl-vrf mgmt set $$
+if [ -z "$VRF" ]; then
+	/usr/cumulus/bin/cl-vrf mgmt status >/dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		VRF=$(/usr/cumulus/bin/cl-vrf identify prompt)
+		if [ -z "$VRF" ]; then
+			sudo /usr/cumulus/bin/cl-vrf mgmt set $$
+		fi
 	fi
 fi
 
