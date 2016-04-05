fbc0d7fd1663e2512fb1db873993bb6d30ad5599
David Ahern <dsa@cumulusnetworks.com>
2016-04-05 14:54:36 -0600

    Set mgmt vrf context for all logins using libpamscript
    Use libpam-script to set the management VRF context at pam
    session open.
    
    With the PAM hook we no longer need to set the vrf context
    via profile which means we can make the sudo entry PASSWD based
    which closes a hole for getting root access without a password.
    
    Since libpam-script is configured by default in the /etc/pam.d/common*
    config files, adding the libpam-script package will generate syslog
    warnings like this:
     pam-script[4794]: can not stat /usr/share/libpam-script/pam_script_ses_close
    
    Since this is the only Cumulus package currently using libpam-script
    it adds hooks for all configured pam steps (auth, acct, ses_open and
    ses_close) though we only use ses_open.
    
    Ticket: CM-10293
    
    Signed-off-by: David Ahern <dsa@cumulusnetworks.com>

--- etc/profile.d/cl_vrf.sh
+++ etc/profile.d/cl_vrf.sh
@@ -1,15 +1,3 @@
-# default task to management VRF if it exists and task is not running
-# in a VRF context
-if [ -z "$VRF" ]; then
-	/usr/cumulus/bin/cl-vrf mgmt status >/dev/null 2>&1
-	if [ $? -eq 0 ]; then
-		VRF=$(/usr/cumulus/bin/cl-vrf identify prompt)
-		if [ -z "$VRF" ]; then
-			sudo /usr/cumulus/bin/cl-vrf mgmt set $$
-		fi
-	fi
-fi
-
 VRF=$(/usr/cumulus/bin/cl-vrf identify prompt)
 export VRF
 
